e972c5385f980a9e061a1a400ff2f404896810c6
huskyhehe <zhou.he1@northeastern.edu>
2023-05-06 01:41:29 -0700

    Add lambda

--- lambda.py
+++ lambda.py
@@ -14,7 +14,6 @@ my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 new_list = list(filter(lambda x: (x%2 == 0) , my_list))
 
 print(new_list)
-
 # Output: [4, 6, 8, 12]
 
 
@@ -25,5 +24,4 @@ my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 new_list = list(map(lambda x: x * 2 , my_list))
 
 print(new_list)
-
 # Output: [2, 10, 8, 12, 16, 22, 6, 24]
